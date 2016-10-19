e053da4249904e1026fdf5d2f4bb7173be741cfa
Chris Clifford and Suzi Rubino <chris@>
2016-10-19 13:17:57 -0700

    add forms for other math functions and js code

--- js/scripts.js
+++ js/scripts.js
@@ -15,12 +15,34 @@ var divi = function(number1, number2) {
 };
 
 // Everything below this line is user interface (or front-end) logic:
+
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
-    $(".output").text(result);
+    $(".output-add").text(result);
+  });
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var result = sub(number1, number2);
+    $(".output-sub").text(result);
+  });
+  $("form#multi").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multi1").val());
+    var number2 = parseInt($("#multi2").val());
+    var result = multi(number1, number2);
+    $(".output-multi").text(result);
+  });
+  $("form#divis").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divis1").val());
+    var number2 = parseInt($("#divis2").val());
+    var result = divi(number1, number2);
+    $(".output-divis").text(result);
   });
 });
