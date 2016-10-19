3607deb90151ba9fc966f2b0afccad351b1bbede
Chris Clifford and Suzi Rubino <suzi@>
2016-10-19 11:17:12 -0700

    Modify index.html, javascript custom script, add bootstrap and jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+
+// Business (or back-end) logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -11,9 +14,13 @@ var divi = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-var result = sub(number1, number2);
-
-alert(result);
+// Everything below this line is user interface (or front-end) logic:
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $(".output").text(result);
+  });
+});
